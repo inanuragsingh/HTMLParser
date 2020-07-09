e67c88402c7a64390e93335e0901dc0859896b61
Anurag Singh Chaudhary <in.anuragsingh@gmail.com>
2020-07-09 22:59:11 +0530

    organizes imports

--- src/main/java/com/parser/HtmlParserApplication.java
+++ src/main/java/com/parser/HtmlParserApplication.java
@@ -1,13 +1,10 @@
 package com.parser;
 
-import java.util.List;
-
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
-import com.parser.udemy.bean.UdemyQuestionBean;
 import com.parser.udemy.parser.UdemyParser;
 
 @SpringBootApplication(scanBasePackages = {"com.parser"})
